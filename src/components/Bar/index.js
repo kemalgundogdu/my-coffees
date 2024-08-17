a37afa385fb19d61b042eb5b67bb1e86156f6e7f
Kemal Gündoğdu <kemal.gundogdu20@ogr.atauni.edu.tr>
2024-08-17 20:31:15 +0300

    filter redesign

--- src/components/Bar/index.js
+++ src/components/Bar/index.js
@@ -26,23 +26,38 @@ function Bar() {
         </div>
       </div>
       <div
-        className={`fixed top-0 ${
-          open === false ? `-right-full` : `right-0`
-        } w-[62%] h-full bg-white transition-all z-20`}
+        className={`fixed left-0 ${
+          open === false ? `-top-full` : `top-0`
+        } w-full h-full bg-white transition-all duration-75 z-20 text-center`}
       >
-        <div className="p-4">
-          <h2 className="font-semibold text-lg">Kafe Seç</h2>
+        <div className="p-4 flex flex-col items-center justify-center h-full w-full">
+          <h2 className="font-semibold text-lg -mt-20 text-center">Kafe Seç</h2>
+          <p className="text-[11px] text-gray-500 -mt-1 text-center">
+            Gitmek istediğin kafeyi seçerek devam et
+          </p>
           <div className="mt-4">
             {/* cafe list */}
-            {
-              cafeList.map((cafe, index) => (
-                <div key={index}>
-                  <input type="checkbox" id={cafe.id} name={cafe.name} value={cafe.name} />
-                  <label htmlFor={cafe.id}>{cafe.name}</label>
-                </div>
-              ))
-            }            
+            {cafeList.map((cafe, index) => (
+              <div
+                key={index}
+                className="flex gap-2 mt-3 justify-center items-center"
+              >
+                <input
+                  type="checkbox"
+                  id={cafe.id}
+                  name={cafe.name}
+                  value={cafe.name}
+                  className="appearance-none w-4 h-4 border-2 border-blue-200 rounded-full bg-white mt-1 checked:bg-blue-500 checked:border-0"
+                />
+                <label className="text-sm" htmlFor={cafe.id}>
+                  {cafe.name}
+                </label>
+              </div>
+            ))}
           </div>
+          <button className="py-1 bg-emerald-500 text-white rounded w-[96%] mx-auto fixed bottom-4">
+            Uygula
+          </button>
         </div>
       </div>
     </>
